e2eeeb8bdaca202d13b8c9036268265fc4d1d6cb
Akcel77 <Akeucel@gmail.com>
2022-09-26 17:44:24 +0200

    first commit

---
+++ _01_first_lesson/hello_world.py
@@ -0,0 +1,36 @@
+# First print
+print("hello world")
+print("-----------")
+
+# Some variable in python 
+welcome = "hello world" # string
+one = 1 # integer
+one_one = 1.1 # float
+is_true = True # boolean True or False
+
+# print welcome var 
+print(welcome)
+print("-----------")
+
+# Rewrite on var
+welcome = "salut le monde"
+print(welcome)
+print("-----------")
+
+# Operator 
+# + -  / * 
+a = 4
+b = 2 
+
+total = a + b
+print(total)
+total = a - b
+print(total)
+total = a * b
+print(total)
+total = a / b
+print(total)
+
+print(total)
+
+
